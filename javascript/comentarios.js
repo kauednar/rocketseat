05c5099629aed39b330ab3ebe4ba0f46eea44abe
Kauê Lima <128862382+kauednar@users.noreply.github.com>
2023-07-14 15:04:40 -0300

    inicio em javascript

---
+++ javascript/comentarios.js
@@ -0,0 +1,12 @@
+// Aqui eu tenho um comentario em linha
+
+/* 
+    Aqui eu tenho um comentário multi linha
+
+    O código abaixo escreve alguma
+    mensagem no devtools
+
+    */
+
+    console.log('Bem vindos ao Starter')
+    console.log('não iria ignorar')
