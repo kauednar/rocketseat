d021d5170e17ca4bbd716a88595806b1a7e0992e
Kauê Lima <128862382+kauednar@users.noreply.github.com>
2023-07-18 18:43:26 -0300

    revisão js

---
+++ javascript/revisaoobject.js
@@ -0,0 +1,12 @@
+//Object
+
+
+// Criação de um objeto
+const person = {
+    name: 'John',
+    age: 30,
+    weight: 88.6,
+    isAdmin: true
+}
+
+console.log(`${person.name} tem ${person.age} anos`)
