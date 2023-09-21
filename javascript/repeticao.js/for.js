a05ffbf102508e884a083d8ab6b6ed5b87cf94b1
Kauê Lima <128862382+kauednar@users.noreply.github.com>
2023-09-21 20:19:10 -0300

    controle de fluxo e estrutura de repetição

---
+++ javascript/repeticao.js/for.js
@@ -0,0 +1,13 @@
+// Estrutura de repetição
+// for
+// break - para a lexecução do loop
+// continue - pula a execução do momento
+
+for(let i = 1; i <= 10; i++) {
+    if(i === 5) {
+        //break or continue
+        continue;
+    }
+
+    console.log(i)
+}
