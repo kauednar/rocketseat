e3aaf1455cd7a76d6e2e729abc2210837489ae90
Kauê Lima <128862382+kauednar@users.noreply.github.com>
2023-09-26 22:26:35 -0300

    operações matematicas

---
+++ js/script.js
@@ -0,0 +1,20 @@
+alert("Vamos fazer um calculo!")
+let numberOne = prompt("Digite o primeiro numero")
+let numberTwo = prompt("Digite o segundo numero")
+
+numberOne = Number(numberOne)
+numberTwo = Number(numberTwo)
+
+const sum = numberOne + numberTwo
+const sub = numberOne - numberTwo
+const multi = numberOne * numberTwo
+const div = numberOne / numberTwo
+const restDiv = numberOne % numberTwo
+
+alert('Soma: ' + sum)
+alert('Subtação: ' + sub)
+alert('Multiplicação: ' + multi)
+alert('Divisão: ' + div)
+alert('Resto da Divisão: ' + restDiv)
+
+
