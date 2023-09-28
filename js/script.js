60e1bf0dd336236ca47c1c5c490a82685ee623d5
Kauê Lima <128862382+kauednar@users.noreply.github.com>
2023-09-27 22:30:37 -0300

    calculo de media do aluno

--- js/script.js
+++ js/script.js
@@ -1,13 +1,20 @@
 alert('Vamos calcular a média do aluno')
 
-let name = prompt('Qual o nome do aluno?')
-let firstGrade = prompt('Qual a primeira nota?')
-let secondGrade = prompt('Qual a segunda nota?')
-let thirdGrade = prompt('Qual a terceira nota?')
+const student = prompt('Qual o nome do aluno?')
+let firstGrade = prompt('Qual a nota da primeira prova?')
+let secondGrade = prompt('Qual a nota da segunda prova?')
+let thirdGrade = prompt('Qual a nota da terceira prova?')
 
 firstGrade = Number(firstGrade)
 secondGrade = Number(secondGrade)
 thirdGrade = Number(thirdGrade)
 
-const average = ((firstGrade + secondGrade + thirdGrade) / 3)
+const average = (Number(firstGrade) + Number(secondGrade) + Number(thirdGrade)) / 3
 
+alert(`A média do aluno ${student} é ${average.toFixed(2)}`)
+
+if (average > 4){
+    alert(`Parabéns ${student}! Você passou no bimestre!`)
+} else {
+    alert(`Que pena ${student}, você não passou no bimestre, mas não desista! Continue estudando!`)
+}
