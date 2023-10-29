64780758a9b3a59faa700fc2d7a798639ece616a
Kauê Lima <128862382+kauednar@users.noreply.github.com>
2023-10-29 01:28:49 -0300

    modularizando-modal

--- jsNivel5/imc-calculator/js/script.js
+++ jsNivel5/imc-calculator/js/script.js
@@ -1,20 +1,9 @@
+import { Modal } from './modal.js'
+
 // variaveis
 const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
-
-const Modal = {
-    wrapper: document.querySelector('.modal-wrapper'),
-    message: modalMessage = document.querySelector('.modal .title span'),
-    buttonClose: document.querySelector('.modal button.close'),
-
-    open () {
-        Modal.wrapper.classList.add('open')
-    },
-    close () {
-        Modal.wrapper.classList.remove('open')
-    }
-}
 
 form.onsubmit = event => {
     event.preventDefault()
@@ -27,10 +16,6 @@ form.onsubmit = event => {
 
     Modal.message.innerText = message
     Modal.open()
-}
-
-Modal.buttonClose.onclick = () => {
-    Modal.close()
 }
 
 function IMC(weight, height) {
