fb8d7b3d2571b407e3492364ceac7ffadb17469c
Kauê Lima <128862382+kauednar@users.noreply.github.com>
2023-10-30 19:04:06 -0300

    IMC-calculator

--- jsNivel5/imc-calculator/js/script.js
+++ jsNivel5/imc-calculator/js/script.js
@@ -1,9 +1,13 @@
 import { Modal } from './modal.js'
+import { AlertError } from "./alert-error.js"
+import { calculateIMC, notANumber} from "./utils.js"
 
-// variaveis
 const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
+
+inputWeight.oninput = () => AlertError.close()
+inputHeight.oninput = () => AlertError.close()
 
 form.onsubmit = event => {
     event.preventDefault()
@@ -11,14 +15,22 @@ form.onsubmit = event => {
     const weight = inputWeight.value
     const height = inputHeight.value
 
-    const result = IMC(weight, height)
+    const weightOrHeightIsNotANumber = notANumber(weight) || notANumber(height)
+
+    if(weightOrHeightIsNotANumber) {
+        AlertError.open()
+        return;
+    }
+
+    const result = calculateIMC(weight, height)
+    displayResultMessage(result)
+}
+
+function displayResultMessage(result) {
     const message = `Seu IMC é de ${result}`
 
     Modal.message.innerText = message
     Modal.open()
 }
 
-function IMC(weight, height) {
-    return (weight / ((height / 100) ** 2)).toFixed(2)
-}
 
