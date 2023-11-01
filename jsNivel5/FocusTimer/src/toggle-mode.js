e4d077feb82f146d04491cd52b84e8cceb133b10
Kauê Lima <128862382+kauednar@users.noreply.github.com>
2023-11-01 16:51:21 -0300

    modulos-focus-timer

---
+++ jsNivel5/FocusTimer/src/toggle-mode.js
@@ -0,0 +1,12 @@
+let darkMode = true
+const buttonToggle = document.getElementById('toggle-mode')
+
+buttonToggle.addEventListener('click', (event) => {
+    document.documentElement.classList.toggle('light')
+    
+    const mode = darkMode ? 'light' : 'dark'
+    event.currentTarget
+        .querySelector('span').textcontent = `${mode} mode ativado!`
+
+    darkMode = !darkMode    
+})
