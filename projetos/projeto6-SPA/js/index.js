d2d21ae8cbe16dc8f1e324d25dcfca1a37738688
Kauê Lima <128862382+kauednar@users.noreply.github.com>
2023-12-20 12:58:57 -0300

    fix

--- projetos/projeto6-SPA/js/index.js
+++ projetos/projeto6-SPA/js/index.js
@@ -1,10 +1,10 @@
 import { Router } from './router.js'
 
 const router = new Router()
-router.add('/', "/pages/home.html")
+router.add('/home', "/pages/home.html")
 router.add('/universe', "/pages/universe.html")
 router.add('/explorer', "/pages/explorer.html")
-router.add(404, "/pages/404.html")
+router.add('/404', "/pages/404.html")
 
 router.handle()
 
